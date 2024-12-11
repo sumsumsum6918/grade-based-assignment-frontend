d1b4468bb33f96510a74aee5d91778a068c7e394
sum <tinafongfong@gmail.com>
2024-12-11 18:45:23 +0100

    make reshake button interactive

---
+++ script/event.js
@@ -0,0 +1,5 @@
+import { loadPage } from "../script/index.js";
+
+export async function handleReshakeButton() {
+  await loadPage();
+}
--- script/index.js
+++ script/index.js
@@ -1,16 +1,24 @@
 import { mapRawCocktailData } from "../script/utilities.js";
 import { getRandomDrink, getDrink } from "../script/api.js";
+import { handleReshakeButton } from "./event.js";
 import { generateIndexHTML, generateDetailsPageHTML } from "./dom.js";
 
-getRandomDrink()
-  .then((res) => {
-    const randomDrinkObject = mapRawCocktailData(res);
-    generateIndexHTML(randomDrinkObject);
-    return randomDrinkObject;
-  })
-  .then((randomDrinkObject) => {
-    getDrink(randomDrinkObject.id).then((res) => {
-      const drinkObject = mapRawCocktailData(res);
-      generateDetailsPageHTML(drinkObject);
+loadPage();
+export async function loadPage() {
+  await getRandomDrink()
+    .then((res) => {
+      const randomDrinkObject = mapRawCocktailData(res);
+      generateIndexHTML(randomDrinkObject);
+
+      return randomDrinkObject;
+    })
+    .then((randomDrinkObject) => {
+      const reshakeButton = document.querySelector(".reshake-button");
+      reshakeButton.addEventListener("click", handleReshakeButton);
+
+      getDrink(randomDrinkObject.id).then((res) => {
+        const drinkObject = mapRawCocktailData(res);
+        generateDetailsPageHTML(drinkObject);
+      });
     });
-  });
+}
